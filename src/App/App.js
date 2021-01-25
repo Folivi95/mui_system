7cb551b4d141e98fc6d6dcd39b6dc9eeab0a06ef
joshuafolivi <joshua.folivi@gmail.com>
2021-01-25 17:30:53 +0100

    added appbar and some components

---
+++ src/App/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import SideMenu from '../Components/SideMenu';
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
+import Header from '../Components/Header';
+import PageHeader from '../Components/PageHeader';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#333996',
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: '#f83245',
+      light: '#f8324526'
+    },
+    background: {
+      default: '#f4f5fd'
+    }
+  },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: 'translateZ(0)'
+      }
+    }
+  }
+})
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%'
+  }
+})
+
+function App() {
+  const classes = useStyles();
+  return (
+    <ThemeProvider theme={theme}>
+      <SideMenu />
+      <div className={classes.appMain}>
+        <Header />
+        <PageHeader 
+          title="Page Header"
+          subTitle="Page Description"
+          icon={<PeopleOutlineIcon fontSize="large" />}
+        />
+      </div>
+      <CssBaseline></CssBaseline>
+    </ThemeProvider>
+
+  );
+}
+
+export default App;
